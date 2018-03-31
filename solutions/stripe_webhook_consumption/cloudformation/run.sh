3b63b1e0ebbf6492e4e9a8768aeb7c7c1cbe5bc7
Eduardo Tenório <embatbr@gmail.com>
2018-03-31 18:51:40 -0300

    GH-7 Adding Lambda function to template.

--- solutions/stripe_webhook_consumption/cloudformation/run.sh
+++ solutions/stripe_webhook_consumption/cloudformation/run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+
+export PROJECT_ROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $PROJECT_ROOT_PATH
+
+
+echo "Deleting..."
 aws cloudformation delete-stack \
     --stack-name apigateway
 
+sleep 5s
+
+cd ../lambda
+zip stripe_webhooks_lambda.zip event_extractor.py
+aws s3 cp stripe_webhooks_lambda.zip s3://stripe-8fit/stripe_webhooks_lambda.zip
+
+cd ../cloudformation
+echo "Creating..."
 aws cloudformation create-stack \
     --stack-name apigateway \
     --template-body file://./template.json \
